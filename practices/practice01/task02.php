e8301759de8956a0f9c65b3a05e5656323a04386
amarkatoliya <amarbharavad994@gmail.com>
2026-01-28 18:58:37 +0530

    PHP-002-Understanding Arrays and Debugging with vars

---
+++ practices/practice01/task02.php
@@ -0,0 +1,14 @@
+<?php
+
+// assosiative array
+$person = [
+    "name" => "Amar",
+    "age" => 24,
+    "skills" => ["singing","dancing","writting"]
+];
+
+// print row structure
+// print_r($person);
+
+var_dump($person);
+?>
